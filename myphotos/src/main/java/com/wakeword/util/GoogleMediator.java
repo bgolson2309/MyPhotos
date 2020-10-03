f3f8bf55580959c2540c20d0e0bece6a06ab1912
bgols <bgols@10.0.0.189>
2020-10-03 18:39:20 -0500

    fix POM

--- myphotos/src/main/java/com/wakeword/util/GoogleMediator.java
+++ myphotos/src/main/java/com/wakeword/util/GoogleMediator.java
@@ -1,10 +1,12 @@
 package com.wakeword.util;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
+
 import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.google.api.client.googleapis.apache.GoogleApacheHttpTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
@@ -16,43 +18,42 @@ public class GoogleMediator {
     private static Logger LOG = getLogger(GoogleMediator.class);
 	private static final String CLIENT_ID = "774522006372-lck9vv9lmtnsi2p64c9oocl6rligfrhu.apps.googleusercontent.com";
 	
+	 
 	public static boolean validatetoken(String token) 
 	{
-		
-		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
-			    .setAudience(Collections.singletonList(CLIENT_ID))
-			    .build();
 
-			GoogleIdToken idToken;
 			try {
-				idToken = verifier.verify(token);
-				if (idToken != null) {
-					  Payload payload = idToken.getPayload();
+				GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
+				    .setAudience(Collections.singletonList(CLIENT_ID))
+				    .build();
+	
+				GoogleIdToken idToken;
 
-					  // Print user identifier
-					  String userId = payload.getSubject();
-					  LOG.debug("GOOGLE User ID: " + userId);
-
-					  // Get profile information from payload
-					  LOG.debug(payload.getEmail());
-					  LOG.debug(payload.getEmail());
-					  LOG.debug((String) payload.get("name"));
-					  LOG.debug((String) payload.get("picture"));
-					  LOG.debug((String) payload.get("locale"));
-					  LOG.debug((String) payload.get("family_name"));
-					  LOG.debug((String) payload.get("given_name"));
-					  return true;
-				} else {
-					LOG.debug("Invalid Google ID token." + token);
-						return false;
-				}
-			} catch (GeneralSecurityException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+//				idToken = verifier.verify(token);
+//				if (idToken != null) {
+//					  Payload payload = idToken.getPayload();
+//
+//					  // Print user identifier
+//					  String userId = payload.getSubject();
+//					  LOG.debug("GOOGLE User ID: " + userId);
+//
+//					  // Get profile information from payload
+//					  LOG.debug(payload.getEmail());
+//					  LOG.debug(payload.getEmail());
+//					  LOG.debug((String) payload.get("name"));
+//					  LOG.debug((String) payload.get("picture"));
+//					  LOG.debug((String) payload.get("locale"));
+//					  LOG.debug((String) payload.get("family_name"));
+//					  LOG.debug((String) payload.get("given_name"));
+//					  return true;
+//				} else {
+//					LOG.debug("Invalid Google ID token." + token);
+//						return false;
+//				}
+			} catch (Exception e) {
+				System.out.println(e.getStackTrace());
+				LOG.debug(e.getStackTrace().toString());
+			}		
 					
 			return false;
 	}
